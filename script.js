f22a29179dbf89346927ce7ee9756184b4d2a477
Christian Zuckero <christianazuckero@gmail.com>
2024-02-21 11:25:28 -0500

    Added function

--- script.js
+++ script.js
@@ -12,15 +12,19 @@ console.log(flavorArray);
 // Create object to store the flavors in
 let flavorObject = {}
 
-// Iterate through the array of flavors to count how many of each flavor there is
-for (let i = 0; i < flavorArray.length; i++) {
-    if (flavorArray[i] in flavorObject) {
-        flavorObject[flavorArray[i]] += 1;
-    } else {
-        flavorObject[flavorArray[i]] = 1;
+// Create a function to iterate through the array of flavors to count how many of each flavor there is
+const flavorCount = (flavorArray) => {
+    for (let i = 0; i < flavorArray.length; i++) {
+        if (flavorArray[i] in flavorObject) {
+            flavorObject[flavorArray[i]] += 1;
+        } else {
+            flavorObject[flavorArray[i]] = 1;
+        }
     }
+    return console.table(flavorObject);
 }
 
-// Log the object into a table
-console.table(flavorObject)
+// Call the function to log the object into a table
+flavorCount(flavorArray);
+
 
